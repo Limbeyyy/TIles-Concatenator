79963787577be90105f1b377c648c62d754c2fc2
Limbeyyy <ingnamrahul@gmail.com>
2026-08-27 17:33:59 +0545

    Add core configuration and globals modules

---
+++ src/app/core/configs.py
@@ -0,0 +1,108 @@
+import numpy as np
+from core.globals import N_H_BOX, TOKEN
+
+
+NO_AUTH_MAP_URL = f"https://kataho.app/api/third-party/{TOKEN}/map-api"
+MAP_URL = f"https://kataho.app/login"
+
+MIN_MAX_LAT = np.array([26.3475375, 29.9728125])
+MIN_MAX_LON = np.array([80.3652031, 88.1864843])
+
+LEVELS = ["country", "district", "municiplaity", "ward", "ward+1", "ward+2"]
+LAYERS_BTN_MAPPING = {
+    "openstreetmap": "openstreetmap",
+    "satellite": "satellite",
+    "terrain": "terrain",
+    "hybrid": "hybrid",
+}
+LAYERS = list(LAYERS_BTN_MAPPING.keys())
+RELOCATE_DIRECTIONS_MAPPING = {"north": 0, "east": 90, "south": 180, "west": 270}
+RELOCATE_DIRECTIONS = list(RELOCATE_DIRECTIONS_MAPPING.keys())
+RELOCATE_DISTANCE = 10000
+
+
+MAP_CONFIG_NO_AUTH = {
+    "country": {"zoom_type": "none", "zoom_value": 0},
+    "district": {"zoom_type": "out", "zoom_value": 2},
+    "municiplaity": {
+        "zoom_type": "out",
+        "zoom_value": 2,
+        "n_h_box": 15,
+        "n_v_box": 5,
+        "crop_left": 85,
+        "crop_right": 86,
+        "crop_top_button_min": 122,  # 118
+    },
+    "ward": {
+        "zoom_type": "out",
+        "zoom_value": 1,
+        "n_h_box": 7,
+        "n_v_box": 2,
+        "crop_left": 144,
+        "crop_right": 144,
+        "crop_top_button_min": 184,
+    },
+    "ward+1": {
+        "zoom_type": "none",
+        "zoom_value": 0,
+        "n_h_box": 3,
+        "n_v_box": 1,
+        "crop_left": 261,
+        "crop_right": 261,
+        "crop_top_button_min": 184,
+    },
+    "ward+2": {
+        "zoom_type": "in",
+        "zoom_value": 1,
+        "n_h_box": 1,
+        "n_v_box": 1,
+        "crop_left": 488,
+        "crop_right": 500,
+        "crop_top": 8,
+        "crop_button": 19,
+        "crop_top_button_min": 188,
+    },
+}
+
+MAP_CONFIG_AUTH = {
+    "country": {"zoom_type": "none", "zoom_value": 0},
+    "district": {"zoom_type": "out", "zoom_value": 2},
+    "municiplaity": {
+        "zoom_type": "in",
+        "zoom_value": 5 - 1,
+        "n_h_box": 15,
+        "n_v_box": 5,
+        "crop_left": 85,
+        "crop_right": 86,
+        "crop_top_button_min": 122,  # 118
+    },
+    "ward": {
+        "zoom_type": "in",
+        "zoom_value": 6 - 1,
+        "n_h_box": 7,
+        "n_v_box": 2,
+        "crop_left": 144,
+        "crop_right": 144,
+        "crop_top_button_min": 188,  # 184
+    },
+    "ward+1": {
+        "zoom_type": "in",
+        "zoom_value": 7 - 1,
+        "n_h_box": 3,
+        "n_v_box": 1,
+        "crop_left": 261,
+        "crop_right": 261,
+        "crop_top_button_min": 188,  # 184
+    },
+    "ward+2": {
+        "zoom_type": "in",
+        "zoom_value": 8 - 1,
+        "n_h_box": 1,
+        "n_v_box": 1,
+        "crop_left": 488,
+        "crop_right": 500,
+        "crop_top": 8,
+        "crop_button": 19,
+        "crop_top_button_min": 188,
+    },
+}
---
+++ src/app/core/globals.py
@@ -0,0 +1,62 @@
+APP_NAME = "Map-Concator-App"
+
+TOP_KODE_SUFFIX = "०१०३"  # "०१९३"  # "०१४३"
+TOP_KODE_SUFFIX_WARD_2 = "००००"
+
+TOKEN = "8128620b-29be-457c-9138-03433c4233f9"
+
+SELECTED_LEVEL = "municipality"
+SELECTED_LAYER = "default"
+SELECTED_DIRECTION = "east"
+RELOCATE_DISTANCE = 10000
+ZOOM_LEVEL = 3
+ZOOM_TYPE = "in"
+
+
+
+LOCAL = False
+OK_CLICK = False
+GRID_CLICK = True
+MAP_SELECT = True
+
+# # distances
+# h_dist =  3.075968283426476
+# v_dist =  2.7798731657884272
+# # h_dist = 3.075968283426476  # 3.075968283426476
+# # v_dist = 2.7798731657884272 # 2.7798731657884272
+
+# H_DIST = 80 * h_dist
+# V_DIST = 100 * v_dist
+
+# map
+MANUAL_CONFIG_DELAY = 5.0
+REFRESH_DURATION = 5.0  # seconds
+
+N_ZOOM_IN = 3
+N_ZOOM_OUT = 2
+
+MIN_LONGITUDE = 80.0
+CROP_FACTOR = 1.25
+
+# Map Control
+R_km = 6378.1370  # Radious of earth
+R_m = R_km * 1000
+
+N_H_SMALL_BOXES = 80
+N_V_SMALL_BOXES = 100
+N_H_BOX = 15
+N_V_BOX = 5
+
+# image crop
+# CROP_TOP = 118
+# CROP_BUTTON = 118
+CROP_TOP_BUTTON_MIN = 118
+CROP_RIGHT = 86
+CROP_LEFT = 85
+
+# UI
+ICONPATH = "media/dev_logo.ico"
+REQ_URL_AFTER_LOGIN = "https://kataho.app/organization"
+
+left_top = (27.8961625, 85.008734375)
+left_top = (27.8964125, 85.007546875)
